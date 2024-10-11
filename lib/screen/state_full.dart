5f5d19725fce817644f3c405a4b67a3817f5fdc3
hsshin <hsshin@euclidsoft.co.kr>
2024-10-11 13:30:14 +0900

    stateful widget commit 20241011 shs

--- lib/screen/state_full.dart
+++ lib/screen/state_full.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:statefulwidget/screen/state_less.dart';
 
 class state_full extends StatefulWidget {
   const state_full({super.key});
 
   @override
-  State<state_full> createState() => _state_full();
+  // State<state_full> createState() => _state_full();
+  State<state_full> createState() => StateFullLifeCycle();
 }
 
 class _state_full extends State<state_full> {
-  Color color = Colors.blue;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +20,18 @@ class _state_full extends State<state_full> {
           children: [
             ElevatedButton(
               onPressed: () {
-                if(color == Colors.red){
+                if (color == Colors.red) {
                   color = Colors.blue;
-                }else if(color == Colors.blue){
+                } else if (color == Colors.blue) {
                   color = Colors.red;
                 }
                 setState(() {});
               },
               child: Text('색상 변경!'),
             ),
-            SizedBox(height: 32.0,),
+            SizedBox(
+              height: 32.0,
+            ),
             Container(
               width: 50.0,
               height: 50.0,
@@ -39,5 +41,117 @@ class _state_full extends State<state_full> {
         ),
       ),
     );
+  }
+}
+
+class StateFullLifeCycle extends State<state_full> {
+  bool show = false;
+  Color color = Colors.red;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (show)
+              GestureDetector(
+                onTap: (){
+                  setState(() {
+                    color = color == Colors.blue ? Colors.red : Colors.blue;
+                  });
+                },
+                child: HashedFactoryWidget(
+                  color: color,
+                ),
+              ),
+            const SizedBox(
+              height: 32.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  show = !show;
+                });
+              },
+              child: const Text('클릭 show'),
+            ),
+
+            // ElevatedButton(
+            //   onPressed: () {
+            //     if(color == Colors.red){
+            //       color = Colors.blue;
+            //     }else if(color == Colors.blue){
+            //       color = Colors.red;
+            //     }
+            //     setState(() {});
+            //   },
+            //   child: Text('색상 변경!'),
+            // ),
+            // SizedBox(height: 32.0,),
+            // Container(
+            //   width: 50.0,
+            //   height: 50.0,
+            //   color: color,
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HashedFactoryWidget extends StatefulWidget {
+  final Color color;
+
+  HashedFactoryWidget({
+    super.key,
+    required this.color,
+  }) {
+    print('StateFull Widget Constructor');
+  }
+
+  @override
+  State<HashedFactoryWidget> createState() {
+    print('StateFull Widget Create State');
+    return _HashedFactoryWidgetState();
+  }
+}
+
+class _HashedFactoryWidgetState extends State<HashedFactoryWidget> {
+  @override
+  void initState() {
+    print('_HashedFactoryWidgetState Widget Init');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('_HashedFactoryWidgetState Widget didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('_HashedFactoryWidgetState Widget build');
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: widget.color,
+    );
+  }
+
+  @override
+  void deactivate() {
+    print('_HashedFactoryWidgetState Widget deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('_HashedFactoryWidgetState Widget dispose');
+    super.dispose();
   }
 }
--- lib/screen/state_less.dart
+++ lib/screen/state_less.dart
@@ -8,31 +8,60 @@ class state_less extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                if(color == Colors.red){
-                  color = Colors.blue;
-                  print('clicked red');
-                }else if(color == Colors.blue){
-                  color = Colors.red;
-                  print('clicked blue');
-                }
-              },
-              child: Text('색상 변경!'),
-            ),
-            SizedBox(height: 32.0,),
-            Container(
-              width: 50.0,
-              height: 50.0,
-              color: color,
-            )
-          ],
-        ),
+      body: HashFactoryWidget(),
+    );
+  }
+}
+
+class HashFactoryWidget extends StatelessWidget {
+  HashFactoryWidget({super.key}){
+    print('----- 생성자 -----');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('----- 빌드 -----');
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: Colors.red,
+    );
+  }
+}
+
+
+class SampleContainor extends StatelessWidget {
+  SampleContainor({super.key}){
+    print('----- 생성자 -----');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              if(color == Colors.red){
+                color = Colors.blue;
+                print('clicked red');
+              }else if(color == Colors.blue){
+                color = Colors.red;
+                print('clicked blue');
+              }
+            },
+            child: Text('색상 변경!'),
+          ),
+          SizedBox(height: 32.0,),
+          Container(
+            width: 50.0,
+            height: 50.0,
+            color: color,
+          )
+        ],
       ),
     );
   }
